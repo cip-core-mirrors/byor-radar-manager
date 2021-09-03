883b5530849f4fb9318f59fe7175668538e2107a
Murat Cengiz <mcengiz2402@gmail.com>
2021-09-03 13:48:53 +0200

    Server to serve files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('build'));
+app.get('/:radarId', async function(req, res, next) {
+    res.sendFile('index.html', {
+        root: './build'
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
