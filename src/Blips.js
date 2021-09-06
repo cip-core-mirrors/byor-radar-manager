3f94b8b59b15bb502b1f4f67e022452d6eae82ad
Murat Cengiz <mcengiz2402@gmail.com>
2021-09-06 19:54:20 +0200

    Add sector and ring / UI only (no backend action)

--- src/Blips.js
+++ src/Blips.js
@@ -11,6 +11,8 @@ class Blips extends React.Component {
         this.reorder = this.reorder.bind(this);
         this.move = this.move.bind(this);
         this.onDragEnd = this.onDragEnd.bind(this);
+        this.newSector = this.newSector.bind(this);
+        this.newRing = this.newRing.bind(this);
 
         this.lists = {};
         Object.assign(this.lists, this.props.blips);
@@ -69,11 +71,20 @@ class Blips extends React.Component {
         this.handleChange();
     };
 
+    newSector(event) {
+        this.lists[`droppable${Object.keys(this.lists).length + 1}`] = [];
+        this.handleChange();
+    }
+
+    newRing(event) {
+        // TODO : add ring here
+        console.log(this.lists);
+    }
+
     render() {
         function getListStyle(isDraggingOver) {
             return {
                 padding: grid,
-                width: 300,
                 borderStyle: 'outset',
                 borderColor: 'var(--gray)',
                 borderWidth: 1,
@@ -102,42 +113,64 @@ class Blips extends React.Component {
             }
         }
 
+        const parent = this;
+
         return (
             <div className="blips">
                 <DragDropContext onDragEnd={this.onDragEnd}>
-                    {Object.entries(this.lists).map(function(entry) {
-                        return <Droppable droppableId={entry[0]} key={entry[0]}>
-                            {(provided, snapshot) => (
-                                <ul
-                                    ref={provided.innerRef}
-                                    style={getListStyle(snapshot.isDraggingOver)}
-                                    className="list-group">
-                                    {entry[1].map((item, index) => (
-                                        <Draggable
-                                            key={item.id_version}
-                                            draggableId={item.id_version}
-                                            index={index}
-                                            className="list-group-item list-group-item-action border-light">
-                                            {(provided, snapshot) => (
-                                                <li
-                                                    ref={provided.innerRef}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-                                                    style={getItemStyle(
-                                                        snapshot.isDragging,
-                                                        provided.draggableProps.style
-                                                    )}>
-                                                    <span className="text-large font-weight-medium">{item.id.substring(0, item.id.lastIndexOf('-'))}</span><br/>
-                                                    <span className="font-weight-normal">{item.name} </span>
-                                                    <span className="text-light">(v{item.version})</span>
-                                                </li>
-                                            )}
-                                        </Draggable>
-                                    ))}
-                                    {provided.placeholder}
-                                </ul>
-                            )}
-                        </Droppable>})
+                    {
+                        Object.entries(this.lists).map(function(entry) {
+                            return <div className="list-grid" key={entry[0]}>
+                                {
+                                    entry[0] === 'droppable1' ?
+                                        <span className="blips-list-label">All blips</span>
+                                        :
+                                        <button className="btn btn-lg btn-flat-primary" onClick={parent.newSector}>
+                                            <i className="icon icon-md">add</i>
+                                            <span className="new-sector-btn">New sector</span>
+                                        </button>
+                                }
+                                <Droppable droppableId={entry[0]}>
+                                    {(provided, snapshot) => (
+                                        <ul
+                                            ref={provided.innerRef}
+                                            style={getListStyle(snapshot.isDraggingOver)}
+                                            className={`list-group ${entry[0] === 'droppable1' ? 'blip-list' : ''}`}>
+                                            {entry[1].map((item, index) => (
+                                                <Draggable
+                                                    key={item.id_version}
+                                                    draggableId={item.id_version}
+                                                    index={index}
+                                                    className="list-group-item list-group-item-action border-light">
+                                                    {(provided, snapshot) => (
+                                                        <li
+                                                            ref={provided.innerRef}
+                                                            {...provided.draggableProps}
+                                                            {...provided.dragHandleProps}
+                                                            style={getItemStyle(
+                                                                snapshot.isDragging,
+                                                                provided.draggableProps.style
+                                                            )}>
+                                                            <span className="text-large font-weight-medium">{item.id.substring(0, item.id.lastIndexOf('-'))}</span><br/>
+                                                            <span className="font-weight-normal">{item.name} </span>
+                                                            <span className="text-light">(v{item.version})</span>
+                                                        </li>
+                                                    )}
+                                                </Draggable>
+                                            ))}
+                                            {provided.placeholder}
+                                        </ul>
+                                    )}
+                                </Droppable>
+                                {
+                                    entry[0] === 'droppable1' ? <div /> :
+                                        <button className="btn btn-lg btn-flat-primary" onClick={parent.newRing}>
+                                            <i className="icon icon-md">add</i>
+                                            <span className="new-sector-btn">New ring</span>
+                                        </button>
+                                }
+                            </div>
+                        })
                     }
                 </DragDropContext>
             </div>
